ac3a92d50540a3265e9fa55c9b5c62dec65d8601
Jonas Søholm Viktorius <jona904u@edu.easj.dk>
2016-04-20 10:10:57 +0200

    MonumentAppen

---
+++ MonumentApp/Pages/WelcomePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace MonumentApp.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class WelcomePage : Page
+    {
+        public WelcomePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MonumentApp/Shell.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Intense.Presentation;
+using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using MonumentApp.Pages;
+using MonumentApp.Presentation;
+
+namespace MonumentApp
+{
+    public sealed partial class Shell : UserControl
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+
+            var vm = new ShellViewModel();
+            vm.TopItems.Add(new NavigationItem { Icon = "", DisplayName = "Forside", PageType = typeof(WelcomePage) });
+            vm.TopItems.Add(new NavigationItem { Icon = "", DisplayName = "Opret Monument", PageType = typeof(OpretMonumentPage) });
+            vm.TopItems.Add(new NavigationItem { Icon = "", DisplayName = "Monument", PageType = typeof(MonumentPage) });
+            vm.TopItems.Add(new NavigationItem { Icon = "", DisplayName = "Page 3", PageType = typeof(Page3) });
+
+            vm.BottomItems.Add(new NavigationItem { Icon = "", DisplayName = "Indstillinger", PageType = typeof(SettingsPage) });
+
+            // select the first top item
+            vm.SelectedItem = vm.TopItems.First();
+
+            this.ViewModel = vm;
+        }
+
+        public ShellViewModel ViewModel { get; private set; }
+
+        public Frame RootFrame
+        {
+            get
+            {
+                return this.Frame;
+            }
+        }
+    }
+}
