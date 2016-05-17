28b6d44edbd9a50b3b72d8cf1327bf692ab914ca
Jonas Søholm Viktorius <jona904u@edu.easj.dk>
2016-05-17 19:49:52 +0200

    Det meste webservice

---
+++ MonumentApp/Handler/MonumentHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonumentApp.ViewModel;
+
+namespace MonumentApp.Handler
+{
+    class MonumentHandler
+    {
+        public MonumentViewModel MonumentViewModel { get; set; }
+
+        public MonumentHandler(MonumentViewModel monumentViewModel)
+        {
+            MonumentViewModel = monumentViewModel;
+        }
+
+        public void OpretMonument()
+        {
+            
+        }
+    }
+}
